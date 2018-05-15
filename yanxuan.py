0b3f262a136452abf7a2e06f465b35c93910fb00
walkblank <hanleilei@outlook.com>
2018-05-15 22:08:49 +0800

    deal with super category , category and item list  separately.

--- yanxuan.py
+++ yanxuan.py
@@ -27,8 +27,9 @@ class YanXuan:
 		self.soup = BeautifulSoup('', 'html.parser')
 		self.pyData = {}
 		self.result = {}
+		self.superCategory = []
 		self.search_CategoryList = []
-		self.search_ItemList = {}
+		self.search_ItemList = []
 
 	def set_payload(self, params):
 		self.payload.update(params)
@@ -42,6 +43,9 @@ class YanXuan:
 		searchSoup = BeautifulSoup(searchPage.text, 'html.parser')
 		# print(search_soup)
 		jsonData = searchSoup.find_all('script')[7].text[15:-2]
+		print(jsonData)
+		print('len = ', len(jsonData))
+		# [7].text[15:-2]
 		self.pyData = json.loads(jsonData)
 		# print(self.py_data)
 
@@ -71,75 +75,9 @@ class YanXuan:
 	def export_resualt(self):
 		pass
 
+
 if __name__ == '__main__':
 	yanxuan = YanXuan()
-	yanxuan.set_payload({'categoryId':'1005001', 'subCategoryId':'1005007'})
+	yanxuan.set_payload({'categoryId':'1005001', 'subCategoryId':'1005007', "timer":'tc'})
 	yanxuan.get_pydata()
 	yanxuan.get_search_category_list()
-#
-# payload = {'categoryId':'1005001', 'subCategoryId':'1005007'}
-# yanxuan_page = requests.get('http://you.163.com/item/list', params = payload)
-# # print(yanxuan_page.text)
-# yanxuan_soup = BeautifulSoup(yanxuan_page.text, 'html.parser')
-# # print(yanxuan_soup)
-# f = open('yanxuan.html', 'w+')
-# f.write(str(yanxuan_soup))
-# print('-start to search-')
-# script_list = yanxuan_soup.find_all('script')
-# target_list = script_list[7]
-# print(type(target_list))
-# json_data = target_list.text[15:-2]
-# print(target_list.text[15:-2])
-# print(type(target_list.text))
-# # sep = target_list.text.split('=')
-# # print(len(sep))
-# # print(sep[2]) # print(sep[3])
-# # json_data = sep[1] +'='+sep[2]+ '=' + sep[3]
-# # f = open('json.txt', 'w+')
-# # f.write(json_data)
-# f.close()
-#
-# python_data = json.loads(json_data)
-# print(python_data.keys())
-# # ['deliveryAreaList', 'currentCategory', 'indexBannerItemVO', 'focusList', 'categoryItemList', 'pathList']
-# # print(type(python_data['categoryItemList']))
-# # print(len(python_data['categoryItemList']))
-# # print(python_data['categoryItemList'][3])
-# # print(type(python_data['categoryItemList'][3]))
-# # categoryItemList   len = 7
-# categoryItemList = python_data['categoryItemList']
-# # for category in  categoryItemList:
-# # 	print(category['category'])
-# # 	if category['category']['name'] == '锅具':
-# # 		# print(category['itemList'])
-# # 		for item in category['itemList']:
-# # 			print(item['name'], item['seoTitle'])
-#
-#
-# earned_money = 0
-# single_best_amount = 0
-# best_sold_item = {}
-# for category in  categoryItemList:
-# 	print(category['category']['name'])
-# 	# print(category['itemList'])
-# 	for item in category['itemList']:
-# 		# print(item)
-# 		print('id:', item['id'], '#', item['rank'], '#', item['name'], '#',item['seoTitle'], '#', item['counterPrice'], '#', item['sellVolume'])
-# 		single_amount = item['counterPrice'] * item['sellVolume']
-# 		if single_amount > single_best_amount:
-# 			single_best_amount = single_amount
-# 			# best_sold_item.clear()
-# 			best_sold_item = item
-# 		earned_money = earned_money + item['counterPrice']*item['sellVolume']
-#
-# print('best sold:', single_best_amount, best_sold_item['id'], '排名:', best_sold_item['rank'], best_sold_item['name'], best_sold_item['seoTitle'], '价格：', best_sold_item['counterPrice'], '卖出:', best_sold_item['sellVolume'])
-# print("earned = :", earned_money)
-# print(categoryItemList[0].keys())
-# # ['filterParams', 'itemList', 'category']
-# print(categoryItemList[0]['category'])
-# print(type(categoryItemList[0]['itemList']))
-# print(len(categoryItemList[0]['itemList']))
-# itemList = categoryItemList[0]['itemList']
-# item = itemList[0]
-# print(type(item))
-# print(item.keys())
