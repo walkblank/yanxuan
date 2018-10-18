9be797068654a9b336e57d52a9b2279809f7e4ae
Harry Han <hanleilei@outlook.com>
2018-10-18 16:45:43 +0800

    add emall class definition

---
+++ eMall.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, field
+from typing import List
+
+
+@dataclass 
+class Item:
+    itemID : str
+    url : str
+    category: str
+    name: str
+    realPrice: int
+    orignalPrice: int
+    description : str
+    shopID: int
+    vendorID: str
+    commentCount: int
+    goodCommentCount: int
+    generalCommentCount: int
+    poorCommentCount: int
+    favorDesc1: str
+    favorDesc2: str
+
+@dataclass
+class Category: 
+    name: str
+    url : str
+    cateID: str
+    itemList : List[Item] = field(default_factory=list)
+
+    def addItem(self, item:Item) -> List[Item]:
+        # self.itemList += item
+        #TODO
+        self.itemList += item
+        # return self.itemList
+    
+@dataclass 
+class EMall: 
+    shopName: str
+    cateList: List[Category] = field(default_factory=list)
